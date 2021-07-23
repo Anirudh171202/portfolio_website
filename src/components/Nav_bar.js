0af976892d0666d7ea62595506b81a31a28c6425
ani171202 <anirudh.sathiya@gmail.com>
2021-07-23 14:04:13 +0530

    gh-pages

---
+++ src/components/Nav_bar.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import logo from "../Assets/icon.png";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+import {
+    AiFillStar,
+    AiOutlineHome,
+    AiOutlineFundProjectionScreen,
+    AiOutlineUser,
+  } from "react-icons/ai";
+
+    import { CgFileDocument } from "react-icons/cg";
+
+
+function NavBar() {
+    const [expand, updateExpanded] = useState(false);
+    const [navColour, updateNavbar] = useState(false);
+  
+    function scrollHandler() {
+      if (window.scrollY >= 20) {
+        updateNavbar(true);
+      } 
+      else {
+        updateNavbar(false);
+      }
+    }
+    window.addEventListener('scroll', scrollHandler);
+
+    return (
+        <NavBar
+            expanded={expand}
+            fixed="top"
+            expand="md"
+            className={navColour ? "sticky" : "navbar"}
+        >
+            <Container>
+                <Navbar.Brand href="/">
+                    <img src={logo} className="img-fluid logo" alt="brand" />
+                </Navbar.Brand>
+                <Navbar.Toggle
+                    aria-controls='responsive-navbar-nav'
+                    onClick={() => {
+                        updateExpanded(expand ? false : "expanded");
+                    }}
+                >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </Navbar.Toggle>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ml-auto" defaultActiveKey="#home">
+                        <Nav.Item>
+                            <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
+                                <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
+                            </Nav.Link>
+                        </Nav.Item>
+
+                    </Nav>
+
+                </Navbar.Collapse>
+
+            </Container>
+
+        </NavBar>
+    );
+}
+
+export default NavBar;
